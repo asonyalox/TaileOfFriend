d20e57d1cba401db3dc2e1c44e0591d532e16e3b
Misha Lytva <asonyalox@gmail.com>
2019-06-30 22:00:22 +0300

    Початок тестування

---
+++ TaileOfFriendTest/UserServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaileOfFriend.BLL.DTO;
+using TaileOfFriend.BLL.Infrasrtucture;
+using TaileOfFriend.BLL.Services;
+using TaileOfFriend.DAL.Enteties;
+using TaileOfFriend.DAL.Interfaces;
+using Xunit;
+
+namespace TaileOfFriendTest
+{
+    
+    public class UserServiceTest
+    {
+        private UserService userService;
+
+        
+
+        [Fact]
+        public async Task<OperationDetails> CreateAsync_EmailNotExisting_ReturnTrue()
+        {
+            //arrange
+            var mock = new Mock<IUnitOfWork>();
+            var service = new UserService(mock.Object);
+            var newUser = new UserDTO { Email = "asonyalox@gmail.com" };
+
+            //act
+            var result = await service.CreateAsync(newUser);
+
+            //assert
+           return Assert.IsType<OperationDetails>(result);
+        }
+    }
+}
